f0822ff0b1163e40bf8ea454e30d492c1f60df99
Kmek <kmekeefe@gmail.com>
2020-09-28 19:23:42 -0400

    Add an interval that will fire every minute

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@
 // Prerequisites:
 const Discord = require("discord.js");
 const config = require ("./config.json");
-const Reminder = require('./Reminder.js');
+const Reminder = require("./Reminder.js");
 const client = new Discord.Client();
 
 // Go!
@@ -22,6 +22,8 @@ client.on("message", (message) => {
     // Prevent bot from reading its own messages
     if (message.author.bot) return;
 
+    console.log(message.channel.id);
+
     // Ping Pong test
     if (message.content.startsWith("ping")) {
         message.channel.send("pong!");
@@ -29,11 +31,21 @@ client.on("message", (message) => {
     
     // Shutdown
     if (message.content === "shutdown") {
-        message.channel.send('Shutting down...').then(m => {
+        message.channel.send("Shutting down...").then(m => {
             console.log("Shutting down...");
+            clearInterval(timer);
             client.destroy();
         });
     }
 });
+
+const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+// A 60 second interval will check if any reminders need to be sent every minute
+var timer = setInterval(function() {
+    var now = new Date();
+    console.log("interval firing " + weekDays[now.getDay()] + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds());
+}, 6000);
+// fixme: remember to change this to 60000 once testing is done
 
 // Start with "node bot.js"
