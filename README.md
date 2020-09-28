d94713cddbd3357262f93aace51b7ed195c5b0bd
Kmek <kmekeefe@gmail.com>
2020-09-27 20:53:51 -0400

    Add shutdown command

--- README.md
+++ README.md
@@ -6,3 +6,10 @@
 - Set myself to busy during class so I don't get notifications in class, change back afterwards
 - Send me a reminder 20 minutes before class starts
 - Add the option to turn it off for the day (like for a holiday or snowday in the middle of the week)
+
+---
+
+## Current Commands:
+
+- `ping` - Pings the bot, who responds with "pong!"
+- `shutdown` - properly closes the bot's program
--- bot.js
+++ bot.js
@@ -4,19 +4,29 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+// Go!
+client.login("token");
+
 // Shows when bot is ready to go
 client.on("ready", () => {
-  console.log("I am ready!");
+    console.log("I am ready!");
+});
+
+// When the bot stops
+client.on("disconnect", function(event){
+    console.log(`The WebSocket has closed and will no longer attempt to reconnect`);
 });
 
 // Ping Pong test
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
-  }
+    if (message.content.startsWith("ping")) {
+        message.channel.send("pong!");
+    } else if (message.content === "shutdown") {
+        message.channel.send('Shutting down...').then(m => {
+            console.log("Shutting down...");
+            client.destroy();
+        });
+    }
 });
-
-// Go!
-client.login("token");
 
 // Start with "node bot.js"
